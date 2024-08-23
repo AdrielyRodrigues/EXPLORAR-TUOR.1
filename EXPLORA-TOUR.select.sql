48ab029c228e1ca29758a75eebc07db638d70089
AdrielyRodrigues <159432866+AdrielyRodrigues@users.noreply.github.com>
2024-08-23 12:03:49 -0300

    Update EXPLORA-TOUR.select.sql

--- EXPLORA-TOUR.select.sql
+++ EXPLORA-TOUR.select.sql
@@ -1,36 +1,35 @@
-#// selecione apenas nomes dos guias que começe com a letra "P": 
+#// 1. selecione apenas nomes dos guias que começe com a letra "P": 
 
 select nome from guias; 
-
 select nome from guias where nome like "P%";
 
 
-#// Contar o Número Total de Turistas:
+#// 2. Contar o Número Total de Turistas:
 
 SELECT COUNT(*) AS total_turistas FROM turistas;
 
-#//  Listar todos os pacotes que utilizam uma forma de pagamento específica
+#// 3. Listar todos os pacotes que utilizam uma forma de pagamento específica
 SELECT * FROM pacotes 
 WHERE formaDePagamento = 'pix';
 
 
 
-#// Verificar o calendário de uma determinada excursão
+#// 4. Verificar o calendário de uma determinada excursão
 SELECT * FROM calendario 
 WHERE FK_excurcoes_cod = (SELECT cod FROM excurcoes WHERE destino = 'Rio de Janeiro');
 
 
-#// Obter detalhes de uma excursão com um valor acima de um determinado limite
+#// 5. Obter detalhes de uma excursão com um valor acima de um determinado limite
 SELECT destino, roteiros, valor FROM excurcoes 
 WHERE valor > 1500;
 
 
-#//  Selecionar o nome e contato de todos os guias cadastrados
+#// 6. Selecionar o nome e contato de todos os guias cadastrados
 SELECT nome, telefone, email FROM guias;
 
 
 
-#// Obter a lista de pacotes avaliados com nota igual ou superior a 4
+#// 7. Obter a lista de pacotes avaliados com nota igual ou superior a 4
 SELECT nome, avaliarGuias FROM pacotes 
 WHERE avaliarGuias >= 3;
 
@@ -38,27 +37,27 @@ WHERE avaliarGuias >= 3;
 
 
 
-#//  Contar o número total de turistas cadastrados
+#// 8. Contar o número total de turistas cadastrados
 SELECT COUNT(*) AS total_turistas 
 FROM turistas;
 
 
 
-#// Obter o valor máximo, mínimo e a média dos valores das excursões
+#// 9. Obter o valor máximo, mínimo e a média dos valores das excursões
 SELECT MAX(valor) AS valor_maximo, 
        MIN(valor) AS valor_minimo, 
        AVG(valor) AS valor_medio
 FROM excurcoes;
 
 
-#//  Selecionar as excursões mais caras para cada destino
+#// 10. Selecionar as excursões mais caras para cada destino
 SELECT destino, MAX(valor) AS valor_maximo 
 FROM excurcoes
 GROUP BY destino;
 
 
 
-#//   Selecionar as hospedagens cujo período (checkin até checkiAlt) é maior que 15 dias
+#// 11. Selecionar as hospedagens cujo período (checkin até checkiAlt) é maior que 15 dias
 SELECT cod, hotel, DATEDIFF(checkiAlt, checkin) AS dias_hospedagem 
 FROM hospedagem 
 WHERE DATEDIFF(checkiAlt, checkin) > 15;
